0ebd92ced5e4527cdb8ecec078f9a1d32e319664
Domenik Jones <d.jones@s-v.de>
2016-09-30 12:22:18 +0200

    Project Init Setup

--- setup.py
+++ setup.py
@@ -12,9 +12,36 @@ setup(
     version="0.1-alpha",
     packages=find_packages(),
     license="MIT License",
-    description="A basic django app to publish models with a workflow and permissions",
+    description="A simple django app to publish models with a workflow and permissions",
     long_description=README,
-    url="https://wwww.s-v.de",
+    url="https://github.com/domenikjones/django-publishing",
+    download_url="https://github.com/domenikjones/django-publishing/tarball/0.1",
     author="Scholz & Volkmer (Domenik Jones)",
     author_email="d.jones@s-v.de",
+    keywords='django model moderation editorial development',
+    install_requires=['django', ],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        # 'Topic :: Software Development :: Django Model Moderation',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.2',
+        # 'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
