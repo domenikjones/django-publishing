c1e1a8c6f1dba103b18304c33a0ae9ddfc6dc215
Domenik Jones <d.jones@s-v.de>
2016-10-04 17:25:11 +0200

    version 0.0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-publishing",
-    version="0.0.3c",
+    version="0.0.4",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
