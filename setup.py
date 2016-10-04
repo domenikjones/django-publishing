19a30e2239d04e3eadef62d22f8495671a0bbcda
Domenik Jones <d.jones@s-v.de>
2016-10-04 11:01:31 +0200

    Updated pypi version to 0.0.2c

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-publishing",
-    version="0.0.2b",
+    version="0.0.2c",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
